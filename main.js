45469349c66afd92c694b1efdb03232103ed0ddc
GastonPicco <117951553+GastonPicco@users.noreply.github.com>
2023-10-09 12:44:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 const logo = document.getElementById('logo'); // Obtén el elemento con el ID 'logo'
 const logoImg = document.getElementById('logoImg')
 const logoTxt = document.getElementById('logoTxt')
-const mainContent = document.getElementById('main-content')
+const mainContent = document.querySelectorAll('.main-content')
 const mainBar = document.getElementById('main-bar')
-const contenedores = document.querySelectorAll('.btn');
-const bigBtns = document.querySelectorAll('.bigbtn')
+const botones = document.querySelectorAll('.btn');
 const selector = document.querySelector('.selector')
-// Botones Grandes
-const bignosotros = document.getElementById('bignosotros')
-const bigasi = document.getElementById('bigasi')
-const bigmarcas = document.getElementById('bigmarcas')
-const bigcontactanos = document.getElementById('bigcontactanos')
-const bigunete = document.getElementById('bigunete')
-const bignovedades = document.getElementById('bignovedades')
+const controlBtn = document.getElementById('control-btn')
 // Botones de barra
 const nosotros = document.getElementById('nosotros')
 const asi = document.getElementById('asi')
@@ -20,114 +13,106 @@ const marcas = document.getElementById('marcas')
 const contactanos = document.getElementById('contactanos')
 const unete = document.getElementById('unete')
 const novedades = document.getElementById('novedades')
+let firstClick = false;
 //
-let selection = false;
+let selection = true;
+
 logo.addEventListener('click', () => {
     logoClickEvent();
 });
-//botones grandes
-bignosotros.addEventListener('click',() => {
-    clickEvent();
-    nosotros.classList.add('active');
-})
-bigasi.addEventListener('click',() =>{
-    clickEvent();
-    asi.classList.add('active');
-})
-bigmarcas.addEventListener('click',() =>{
-    clickEvent();
-    marcas.classList.add('active');
-})
-bigcontactanos.addEventListener('click',() =>{
-    clickEvent();
-    contactanos.classList.add('active');
-})
-bigunete.addEventListener('click',() =>{
-    clickEvent();
-    unete.classList.add('active');
-})
-bignovedades.addEventListener('click',() =>{
-    clickEvent();
-    novedades.classList.add('active');
-})
-//botones barra
+
 nosotros.addEventListener('click',() => {
+    btnID = 0;
     cambiarBoton(nosotros);
 })
 asi.addEventListener('click',() =>{
+    btnID = 1;
     cambiarBoton(asi);
 })
 marcas.addEventListener('click',() =>{
+    btnID = 2;
     cambiarBoton(marcas);
 })
 contactanos.addEventListener('click',() =>{
+    btnID = 3;
     cambiarBoton(contactanos);
 })
 unete.addEventListener('click',() =>{
+    btnID = 4;
     cambiarBoton(unete);
 })
 novedades.addEventListener('click',() =>{
+    btnID = 5;
     cambiarBoton(novedades);
 })
 
 
-
 // FUNCIONES
-
+// Selecciona un boton
 function cambiarBoton(boton){
     nosotros.classList.remove('active');
     asi.classList.remove('active');
     marcas.classList.remove('active')
     contactanos.classList.remove('active')
     unete.classList.remove('active')
-    novedades.classList.remove('active')
-
+    novedades.classList.remove('active') 
     boton.classList.add('active')
+
+    ActualizarContenedor(btnID)
 }
 
-function mostrarBigBtn(index) {
+/*function mostrarBigBtn(index) {
     setTimeout(() => {
         bigBtns[index].classList.toggle('show');
     }, index * 18); // 500ms de retraso entre cada contenedor (ajustable)
-}
-function mostrarContenedor(index) {
+}*/
+function mostrarBotones(index) {
     setTimeout(() => {
-        contenedores[index].classList.toggle('show');
-    }, index * 100); // 500ms de retraso entre cada contenedor (ajustable)
-}
-function clickEvent(){
-    if(selection == false){
-        mainContent.classList.toggle('notclicked');
-        selector.classList.toggle('clicked');
-        mainBar.classList.toggle('notclicked');
-        for (let i = 0; i < bigBtns.length; i++) {
-         mostrarBigBtn(i);
-        }
-     for (let i = 0; i < contenedores.length; i++) {
-         mostrarContenedor(i);
-        }
-     selection = true;
-     console.log(selection)
-    }
+        botones[index].classList.toggle('show');
+    }, index * 100);
+    mainBar.classList.toggle('notclicked')
 }
 
 function logoClickEvent(){
-    console.log("click")
-    logo.classList.toggle('clicked'); // Agrega o quita la clase 'clicked' al hacer clic
-    logoImg.classList.toggle('clicked');
-    logoTxt.classList.toggle('clicked');
-    selector.classList.toggle('clicked');
-    
 
-    if(selection == false){
-        for (let i = 0; i < bigBtns.length; i++) {
-            mostrarBigBtn(i);
+    logo.classList.toggle('clicked'); // Agrega o quita la clase 'clicked' al hacer clic
+    logoTxt.classList.toggle('clicked');
+    mainBar.classList.toggle('notclicked')
+    controlBtn.classList.toggle('clicked')
+    for (let i = 0; i < botones.length; i++) {
+        mostrarBotones(i);
+    }
+    if(logo.className.match("clicked") && firstClick==false){
+        firstClick = true;
+        console.log("clickeado");
+        btnID = 0;
+        cambiarBoton(nosotros);
+    }
+    else if(logo.className.match("clicked") && firstClick==true){
+        ActualizarContenedor(btnID);
+    }
+    else{
+        for (let i = 0; i < mainContent.length; i++) {
+            mainContent[i].classList.add('notclicked')
         }
     }
-    if(selection == true){
-        mainContent.classList.toggle('notclicked');
-        for (let i = 0; i < contenedores.length; i++) {
-            mostrarContenedor(i);
-        }
+}
+
+function ActualizarContenedor(id){
+    
+    for (let i = 0; i < mainContent.length; i++) {
+        mainContent[i].classList.add('notclicked')
+        mainContent[i].classList.remove('left')
+        mainContent[i].classList.remove('right')
     }
+    for (let i = 0; i < id; i++){
+        mainContent[i].classList.add('left')
+    }
+    for (let i = id+1;i < mainContent.length; i++){
+        mainContent[i].classList.add('right')
+    }
+    mainContent[id].classList.remove('notclicked')
+    mainContent[id].classList.remove('left')
+    mainContent[id].classList.remove('right')
+    
 }
